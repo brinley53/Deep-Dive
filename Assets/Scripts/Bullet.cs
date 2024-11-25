8438e2152c0b39f35a4eae79153197cce9788384
brinley53 <brinleyhull53@ku.edu>
2024-11-24 19:36:48 -0600

    comments

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,5 +1,7 @@
 /* 
-Author:
+Bullet.cs
+Description: File that controls the harpoon bullets when they get shot
+Authors: Ben Renner, Brinley Hull, Connor Bennudriti, Gianni Louisa, Kyle Moore
 Creation date: 11/23/24
 Other sources of code: Muddy Wolf (youtube)
 */
@@ -13,19 +15,19 @@ public class Bullet : MonoBehaviour
     private float speed = 12f; // Speed of the harpoons
     private float timeAlive = 3f; // Time the bullet stays defined if it hasn't hit anything
 
-    private Rigidbody2D rb;
+    private Rigidbody2D rb; // The rigidbody that controls the actual harpoon object
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        Destroy(gameObject, timeAlive);
+        rb = GetComponent<Rigidbody2D>(); // Creates the rigidbody by getting the harpoon spawned from the game
+        Destroy(gameObject, timeAlive); // Destroys the harpoon after the time specified to stay alive
     }
 
     // Update is called once per frame
     [System.Obsolete]
     void FixedUpdate()
     {
-        rb.velocity = transform.right * speed;
+        rb.velocity = transform.right * speed; // Shoot the harpoon forward 
     }
 }
