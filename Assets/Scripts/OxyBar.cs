47ba7c9888b4c0ee911cba894df5da0df9380220
cbennudr <cbennudriti@hotmail.com>
2024-12-08 20:38:16 -0600

    Added commments to OxyBar.cs

--- Assets/Scripts/OxyBar.cs
+++ Assets/Scripts/OxyBar.cs
@@ -1,6 +1,24 @@
 /*
+OxyBar.cs
+Description: script for handling the oxygen bar
 Creation date: 12/5/2024
+Authors: Brinley Hull
 Other sources of code: weeklyhow.com
+
+Revisions
+* None
+Preconditions:
+* Script must be attached to the oxygen slider object
+Postconditions:
+* None
+Error and Exception conditions:
+* None
+Side effects:
+* None
+Invariants:
+* None
+Known Faults:
+* None
 */
 
 using System.Collections;
@@ -10,15 +28,16 @@ using UnityEngine.UI;
 public class OxyBar : MonoBehaviour
 {
 
-    public Slider oxygenBar;
+    public Slider oxygenBar; // the slider for dispaying the oxygen
 
     private void Start()
     {
-        oxygenBar = GetComponent<Slider>();
-        oxygenBar.maxValue = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>().maxOxygen;
-        oxygenBar.value = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>().oxygen;
+        oxygenBar = GetComponent<Slider>(); // get the slider for the oxygen bar
+        oxygenBar.maxValue = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>().maxOxygen; // set the max value for the slider
+        oxygenBar.value = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerInventory>().oxygen; // set starting alue for the slider
     }
 
+    // Function to set the oxygen level of the slider to a provided int
     public void SetOxygen(int oxy) {
         oxygenBar.value = oxy;
     }
